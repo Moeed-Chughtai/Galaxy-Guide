c23b323c65a221219a58c79fceb48f20f9667ac3
v78685hk <v78685hk@student.manchester.ac.uk>
2024-04-13 13:34:53 +0100

    Fixed camera angle so camer can take a rotatable view - added white plane for reference

--- client/Hackathon/src/components/Three.jsx
+++ client/Hackathon/src/components/Three.jsx
@@ -1,6 +1,7 @@
 import * as THREE from 'three';
 
 import { useEffect, useRef } from "react";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 function MyThree() {
   const refContainer = useRef(null);
@@ -12,19 +13,35 @@ function MyThree() {
 
     var scene = new THREE.Scene();
     var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    
+    //3d camera orbit view thing
+    const controls = new OrbitControls(camera, refContainer.current); //refContainer.current
+    
     var renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild( renderer.domElement );
+
+    camera.position.set(50, 50, 100);
+    controls.update();
 
     // use ref as a mount point of the Three.js scene instead of the document.body
     refContainer.current && refContainer.current.appendChild( renderer.domElement );
 
 
-    // var geometry = new THREE.BoxGeometry(1, 1, 1);
-    // var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    // var cube = new THREE.Mesh(geometry, material);
-    // scene.add(cube);
     
+    //plane geomreyr
+    const planeGeo = new THREE.PlaneGeometry(30, 30);
+    const planeMat = new THREE.MeshBasicMaterial({
+        color: 0xffffff,
+        side: THREE.DoubleSide
+    });
+    const plane = new THREE.Mesh(planeGeo, planeMat);
+
+    scene.add(plane);
+
+    //grid
+
+    const gridHelper = new THREE.GridHelper(30);
 
 
 
@@ -35,10 +52,22 @@ function MyThree() {
     const sphere = new THREE.Mesh(geo, mat);
     scene.add(sphere);
 
-    camera.position.z = 50;
-    
+    // camera.position.z = 50;
+    // camera.position.set(50, 50, 100);
 
-    renderer.render(scene, camera);
+    camera.position.set(50, 50, 100);
+    // controls.update();
+
+    var animate = function () {
+      requestAnimationFrame(animate);
+    
+      //update camera dependant on mouse position
+      controls.update();
+    
+      renderer.render(scene, camera);
+    };
+    animate();
+
   }, []);
   return (
     <div ref={refContainer}></div>
