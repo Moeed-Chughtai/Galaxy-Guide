c266171e18f3f09c4261f043e3679b97f97d2eaa
Muhammad Kalmani <muhammadkalmani@Muhammads-MacBook-Pro-3.local>
2024-04-13 19:06:18 +0100

    Popup css close to finished

--- client/Hackathon/src/Home.jsx
+++ client/Hackathon/src/Home.jsx
@@ -17,6 +17,12 @@ function Home() {
                 <button onClick={() => togglePopup(1)}>Sun</button>
                 <button onClick={() => togglePopup(2)}>Mercury</button>
                 <button onClick={() => togglePopup(3)}>Venus</button>
+                <button onClick={() => togglePopup(4)}>Earth</button>
+                <button onClick={() => togglePopup(5)}>Mars</button>
+                <button onClick={() => togglePopup(6)}>Jupiter</button>
+                <button onClick={() => togglePopup(7)}>Saturn</button>
+                <button onClick={() => togglePopup(8)}>Uranus</button>
+                <button onClick={() => togglePopup(9)}>Neptune</button>
             </div>
             <Popup trigger={showPopup} setTrigger={setShowPopup} planet={selectedPlanet} />
         </div>
--- client/Hackathon/src/components/Popup.jsx
+++ client/Hackathon/src/components/Popup.jsx
@@ -5,9 +5,13 @@ import './../css/popup_style.css';
 function Popup({ trigger, setTrigger, planet }) {
     const planetData = planetsData.find(item => item.id === planet);
 
+    const handleClose = () => {
+        setTrigger(false);
+    };
+
     return trigger ? (
         <div className="popup-frame">
-            <button className="button" onClick={() => setTrigger(false)}>
+            <button className="close-button" onClick={() => setTrigger(false)}>
                 <span className="X"></span>
                 <span className="Y"></span>
                 <div className="close">Close</div>
@@ -17,10 +21,15 @@ function Popup({ trigger, setTrigger, planet }) {
                     <h1 className='planet-title'>{planetData.name}</h1>
                     <h1 className='planet-header'>Information on {planetData.name}</h1>
                     <p className='planet-body'>{planetData.body_paragraph}</p>
+                    <button className='defend-button'>Defend</button>
                 </div>
                 <div className='right-container'>
                     <h1 className='planet-header'>{planetData.name} Facts</h1>
-                    <p className='planet-facts'>{planetData.facts}</p>
+                    <ul className='planet-facts'>
+                        {planetData.facts.map((fact, index) => (
+                            <li key={index}>• {fact}</li>
+                        ))}
+                    </ul>
                 </div>
 
             </div>
