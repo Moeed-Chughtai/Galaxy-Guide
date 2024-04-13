28d47ffd4061b8b2e935ab17002ab329f5dc787d
v78685hk <v78685hk@student.manchester.ac.uk>
2024-04-13 12:46:05 +0100

    Added sphere instead - basic material

--- client/Hackathon/src/components/Three.jsx
+++ client/Hackathon/src/components/Three.jsx
@@ -18,19 +18,27 @@ function MyThree() {
 
     // use ref as a mount point of the Three.js scene instead of the document.body
     refContainer.current && refContainer.current.appendChild( renderer.domElement );
-    var geometry = new THREE.BoxGeometry(1, 1, 1);
-    var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    var cube = new THREE.Mesh(geometry, material);
-    scene.add(cube);
-    camera.position.z = 5;
 
-    var animate = function () {
-      requestAnimationFrame(animate);
-      cube.rotation.x += 0.01;
-      cube.rotation.y += 0.01;
-      renderer.render(scene, camera);
-    };
-    animate();
+
+    // var geometry = new THREE.BoxGeometry(1, 1, 1);
+    // var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    // var cube = new THREE.Mesh(geometry, material);
+    // scene.add(cube);
+    
+
+
+
+    const geo = new THREE.SphereGeometry(20, 20, 20);
+    const mat = new THREE.MeshBasicMaterial({
+        color: 0x00ff00
+    });
+    const sphere = new THREE.Mesh(geo, mat);
+    scene.add(sphere);
+
+    camera.position.z = 50;
+    
+
+    renderer.render(scene, camera);
   }, []);
   return (
     <div ref={refContainer}></div>
