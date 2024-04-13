a75f4c167de3630af90e88424732004f8a2eab20
v78685hk <v78685hk@student.manchester.ac.uk>
2024-04-13 12:24:28 +0100

    Test object rendered - rendered twice

--- client/Hackathon/src/app.jsx
+++ client/Hackathon/src/app.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 
+import MyThree from './components/Three';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+
+    <MyThree/>
+
   </React.StrictMode>,
 )
