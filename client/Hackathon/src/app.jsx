8e5a1b3862c4b6b5a349d9e7e5823992cc361489
Muhammad Kalmani <muhammadkalmani@Muhammads-MacBook-Pro-3.local>
2024-04-13 19:52:01 +0100

    Defend button implemented

--- client/Hackathon/src/app.jsx
+++ client/Hackathon/src/app.jsx
@@ -9,8 +9,8 @@ import MyThree from './components/Three';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
 
-    {/* <MyThree/> */}
-    <Astronaut text="Hi, I'm from Mars" />
+    <MyThree/>
+    {/* <Astronaut text="Hi, I'm from Mars" /> */}
 
   </React.StrictMode>,
 )
--- client/Hackathon/src/components/Popup.jsx
+++ client/Hackathon/src/components/Popup.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import planetsData from './../data/planets_info.json';
 import './../css/popup_style.css';
 
@@ -9,9 +9,13 @@ function Popup({ trigger, setTrigger, planet }) {
         setTrigger(false);
     };
 
+    const handleDefend = () => {
+        setTrigger(false);
+    }
+
     return trigger ? (
         <div className="popup-frame">
-            <button className="close-button" onClick={() => setTrigger(false)}>
+            <button className="close-button" onClick={handleClose}>
                 <span className="X"></span>
                 <span className="Y"></span>
                 <div className="close">Close</div>
@@ -21,7 +25,7 @@ function Popup({ trigger, setTrigger, planet }) {
                     <h1 className='planet-title'>{planetData.name}</h1>
                     <h1 className='planet-header'>Information on {planetData.name}</h1>
                     <p className='planet-body'>{planetData.body_paragraph}</p>
-                    <button className='defend-button'>Defend</button>
+                    <button className='defend-button' onClick={handleDefend}>Defend</button>
                 </div>
                 <div className='right-container'>
                     <h1 className='planet-header'>{planetData.name} Facts</h1>
@@ -31,7 +35,6 @@ function Popup({ trigger, setTrigger, planet }) {
                         ))}
                     </ul>
                 </div>
-
             </div>
         </div>
     ) : null;
